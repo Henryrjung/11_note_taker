00f03a9618b10bf5139d9d6e116e9ed8b94dedfc
HenryJ <henryjung667@gmail.com>
2020-11-30 13:53:52 -0600

    set up initial routes to be filled in later

--- routes.js
+++ routes.js
@@ -4,5 +4,15 @@ const fs = require("fs");
 
 const app = express();
 const mainDirectory = path.join(__dirname, "./public");
+//html routes
+app.get({});
+app.get({});
+app.get({});
+//api routes
+app.get({});
+//new note
+app.post({});
+// delete note
+app.delete({});
 
 module.exports = app;

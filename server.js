d2cfe99c5b20d3c0239fd496c11df08b26fbc151
HenryJ <henryjung667@gmail.com>
2020-11-30 13:46:32 -0600

    created server on port 8080

--- server.js
+++ server.js
@@ -2,13 +2,8 @@ const express = require("express");
 const routes = require("./routes");
 
 const app = express();
-const port = 8080;
+const PORT =  process.env.PORT || 8080;
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-app.use(express.static('./public'));
-app.use(routes);
-
-app.listen(port, function() {
-    console.log(`Server running on port ${port}`);
-})
+app.listen(PORT, function() {
+    console.log(`Server is running on port ${PORT}`);
+});

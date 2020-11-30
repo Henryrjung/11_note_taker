cd8d7f17eea056f20835d8345ba13eb31bf62f0e
HenryJ <henryjung667@gmail.com>
2020-11-30 16:50:40 -0600

    added API routes

--- routes.js
+++ routes.js
@@ -4,24 +4,49 @@ const fs = require("fs");
 
 const app = express();
 const mainDirectory = path.join(__dirname, "./public");
+
+let savedEntries = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
 //html routes
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(mainDirectory, "notes.html"));
 });
 app.get("/api/notes", function(req, res) {
-    res.sendFile(path,join(__dirname, "./db/db.json"));
+    res.sendFile(path.join(__dirname, "./db/db.json"));
 });
 app.get("/api/notes/:id", function(req, res) {
-    const saves = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    res.json(saves[Number(req.params.id)]);
+    res.json(savedEntries[Number(req.params.id)]);
 });
 //api routes
 app.get("*", function(req, res) {
     res.sendFile(path.join(mainDirectory, "index.html"));
 });
 //new note
-app.post({});
+app.post("/api/notes", function(req, res) {
+    let newEntry = req.body;
+    let entryId = (savedEntries.length).toString();
+    newEntry.id = entryId;
+    savedEntries.push(newEntry);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedEntries));
+    console.log("Note saved to db.json. Content: ", newEntry);
+    res.json(savedEntries);
+});
 // delete note
-app.delete({});
+app.delete("/api/notes/:id", function(req, res) {
+    let currentId = req.params.id;
+    let newId = 0;
+    savedEntries = savedEntries.filter(currentEntry => {
+        return currentEntry.id != currentId;
+    })
+    
+    for (currentEntry of savedEntries) {
+        currentEntry.id = newId.toString();
+        newId++;
+    }
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedEntries));
+    res.json(savedEntries);
+});
 
 module.exports = app;
--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const routes = require("./routes");
 const app = express();
 const PORT =  process.env.PORT || 8080;
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('./public'));
+app.use(routes);
+
 app.listen(PORT, function() {
     console.log(`Server is running on port ${PORT}`);
 });
